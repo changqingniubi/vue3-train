97e311b0efa1fa60b68cd6bb1c6013a3d9597a00
changqing <qing.chang@eeoa.com>
2021-12-18 22:34:47 +0800

    生成虚拟dom

---
+++ packages/runtime-core/src/createVNode.ts
@@ -0,0 +1,38 @@
+/*
+ * @Description: 
+ * @Author: changqing
+ * @Date: 2021-12-18 22:17:44
+ * @LastEditTime: 2021-12-18 22:31:54
+ * @LastEditors: changqing
+ * @Usage: 
+ */
+import { isObject, isString, ShapeFlags } from "@vue/shared";
+export function createVNode(type, props, children = null) { // h('div',{},['helloworld','helloworld'])
+    // 虚拟节点就是 用一个对象来描述信息的  
+
+    // & | 
+    const shapeFlag = isObject(type) ?
+        ShapeFlags.COMPONENT :
+        isString(type) ?
+            ShapeFlags.ELEMENT :
+            0
+
+    const vnode = { // 跨平台
+        __v_isVNode: true,
+        type,
+        shapeFlag,
+        props,
+        children,
+        key: props && props.key, // key值
+        component: null, // 如果是组件的虚拟节点要保存组件的实例
+        el: null, // 虚拟节点对应的真实节点
+    }
+    if(children){
+      
+        // 告诉此节点 是什么样的儿子 
+        // 稍后渲染虚拟节点的时候 可以判断儿子是数组 就循环渲染
+        vnode.shapeFlag =  vnode.shapeFlag | (isString(children) ? ShapeFlags.TEXT_CHILDREN:ShapeFlags.ARRAY_CHILDREN)
+    }
+    // vnode 就可以描述出来 当前他是一个什么样的节点 儿子是什么样的
+    return vnode; // createApp(App)
+}
---
+++ packages/runtime-dom/src/index.ts
@@ -0,0 +1,40 @@
+/*
+ * @Description: 
+ * @Author: changqing
+ * @Date: 2021-12-18 21:19:59
+ * @LastEditTime: 2021-12-18 22:07:29
+ * @LastEditors: changqing
+ * @Usage: 
+ */
+// 需要涵盖我们的 dom操作的api 属性操作的api  ， 将这些api 传入到 我们的runtime-core中
+// runtime-core 在操作中不需要依赖于平台代码 （平台代码是被传入的）
+// 我们在渲染页面的时候 需要节点操作的一系列方法
+
+import { createRenderer } from '@vue/runtime-core';
+import { nodeOps } from './nodeOps';
+import { patchProp } from './patchProp';
+
+const renderOptions = Object.assign(nodeOps, { patchProp }); // 包含所需要的所有api
+
+
+// 实现将renderOptions 传入到core中
+// runtime-dom  在这层 对我们浏览器的操作做了一些
+export const createApp = (component, rootProps = null) => {
+    // 需要创建一个渲染器
+    const { createApp } = createRenderer(renderOptions); // runtime-core中的方法
+    let app = createApp(component, rootProps);
+    let { mount } = app; // 获取core中mount
+    app.mount = function (container) {  // 在重写mount
+        container = nodeOps.querySelector(container);
+        container.innerHTML = '';
+        mount(container); // 处理节点后传入到mount中
+    }
+    return app;
+}
+
+export const createSSRApp = () =>{ 
+
+}
+
+export * from '@vue/runtime-core'; // 导出这个模块中的所有代码  es6 模块规范
+
